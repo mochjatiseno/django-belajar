17fe5c588ec5bcd5c89e8a8fd2b5d51d0bb7c770
mochjatiseno <mochjatiseno@gmail.com>
2018-12-10 22:51:14 +0700

    latihan css

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import include
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
 ]
